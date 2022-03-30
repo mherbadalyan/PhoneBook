22b6afc71028c5f82455234d9567e5cf313b94ac
Mher <mherbadalyan@gmail.com>
2022-03-30 13:15:10 +0400

    add validators

--- src/com/company/validators/Validator.java
+++ src/com/company/validators/Validator.java
@@ -1,26 +1,36 @@
 package com.company.validators;
 
+import java.util.regex.Pattern;
+
 public class Validator {
-    public static boolean validFirstMenu(String name){
-        return true;
-    }
-    public static boolean validName(String name){
-        return true;
+    private static final Pattern PHONE_NUMBER_PATTERN = Pattern.compile("[0-9]+");
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9.-]+$");
+
+    public static boolean validName(String name) {
+
+        return name != null;
     }
 
-    public static boolean validSecondMenu(String inputCreateMenu) {
-        return true;
+    public static boolean validFirstMenu(String choose) {
+        return choose.equals("1") || choose.equals("2") || choose.equals("3") || choose.equals("Q");
     }
 
-    public static boolean validPhoneNumber(String phoneNumber) {
-        return true;
+    public static boolean validSecondMenu(String choose) {
+        return validFirstMenu(choose) || choose.equals("4");
     }
 
-    public static boolean createMenuValidator(String operation) {
-        return true;
+    public static boolean createMenuValidator(String choose) {
+        return validFirstMenu(choose) || choose.equals("S");
+    }
+
+    public static boolean validPhoneNumber(String number) {
+        if (number.charAt(0) == '+') {
+            number = number.substring(1);
+        }
+        return number.matches(PHONE_NUMBER_PATTERN.pattern());
     }
 
     public static boolean validEmail(String email) {
-        return true;
+        return email.matches(EMAIL_PATTERN.pattern());
     }
 }
